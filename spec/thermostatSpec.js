82e5734528054bbcc81209f0370af1e69d116dff
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-14 12:01:12 +0100

    Thermostat starts at 20

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,4 +3,11 @@ describe("Thermostat", function(){
     var thermostat = new Thermostat();
     expect(thermostat.temp()).toBe(20);
   });
+
+  it("can be increased by 1 degree", function() {
+    var thermostat = new Thermostat();
+    thermostat.up();
+    expect(thermostat.temp()).toBe(21);
+  });
+
 });
