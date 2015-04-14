5dc646fec7dc6fb9229c35da8122cf471f014bf5
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-14 14:17:39 +0100

    thermostat resets to 25 when changing to powersaving and above 25 degrees

--- src/thermostat.js
+++ src/thermostat.js
@@ -23,3 +23,10 @@ Thermostat.prototype.down = function() {
 Thermostat.prototype.powerSavingOff = function() {
     this.powerSaving = false;
 };
+
+Thermostat.prototype.powerSavingOn = function() {
+  if (this.temp > 25) {
+    this.temp = 25;
+  };
+  this.powerSaving = true;
+};
