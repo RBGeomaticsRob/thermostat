cdb37df7b99b6c38099bb40797b7881ee11adbcd
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-14 17:28:49 +0100

    Added web functionaility to adjust temperature

--- spec/webFeatureSpec.js
+++ spec/webFeatureSpec.js
@@ -1,7 +1,11 @@
 describe("thermostat.html", function() {
-  it("displays the temperature", function() {
+
+  beforeEach(function(){
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('thermostat.html');
+  });
+
+  it("displays the temperature", function() {
     expect("#temperature").toContainHtml("20");
   });
 
@@ -9,7 +13,7 @@ describe("thermostat.html", function() {
     $("#up").click();
     $("#up").click();
     $("#up").click();
-    expect("#temperature").toContainHtml("23")
+    expect("#temperature").toContainHtml("23");
     $("#down").click();
     $("#down").click();
     $("#down").click();
@@ -17,4 +21,10 @@ describe("thermostat.html", function() {
     expect("#temperature").toContainHtml("19")
   });
 
+  it("has a powersaving mode controlled by a tick box", function(){
+    expect($('#power_saving_mode').is(':checked')).toBe(true);
+    expect(thermostat.powerSaving).toBe(true);
+    $("#power_saving_mode").prop('checked', false);
+    expect(thermostat.powerSaving).toBe(false);
+  });
 });
