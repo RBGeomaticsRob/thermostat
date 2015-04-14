4a48a2977c211a8b8d50026c23886e9bcb54c13f
Dan Blakeman <DanBlakeman@users.noreply.github.com>
2015-04-14 16:27:27 +0100

    Temperature is displayed on a webpage

--- spec/webFeatureSpec.js
+++ spec/webFeatureSpec.js
@@ -2,6 +2,19 @@ describe("thermostat.html", function() {
   it("displays the temperature", function() {
     jasmine.getFixtures().fixturesPath = '.';
     loadFixtures('thermostat.html');
-    expect("#temperature").toContain("20");
+    expect("#temperature").toContainHtml("20");
   });
+
+  it("can increase the temperature by three and descrease by four", function() {
+    $("#up").click();
+    $("#up").click();
+    $("#up").click();
+    expect("#temperature").toContainHtml("23")
+    $("#down").click();
+    $("#down").click();
+    $("#down").click();
+    $("#down").click();
+    expect("#temperature").toContainHtml("19")
+  });
+
 });
