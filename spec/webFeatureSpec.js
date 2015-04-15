f05ecd0ce5b025068eb77e6d527e4464135ddb4c
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-15 11:52:02 +0100

    Added a reset button

--- spec/webFeatureSpec.js
+++ spec/webFeatureSpec.js
@@ -7,6 +7,7 @@ describe("thermostat.html", function() {
 
   it("displays the temperature", function() {
     expect("#temperature").toContainHtml("20");
+    expect("#temperature").toContainHtml(thermostat.temp);
   });
 
   it("can increase the temperature by three and descrease by four", function() {
@@ -14,11 +15,13 @@ describe("thermostat.html", function() {
     $("#up").click();
     $("#up").click();
     expect("#temperature").toContainHtml("23");
+    expect("#temperature").toContainHtml(thermostat.temp);
     $("#down").click();
     $("#down").click();
     $("#down").click();
     $("#down").click();
     expect("#temperature").toContainHtml("19")
+    expect("#temperature").toContainHtml(thermostat.temp);
   });
 
   it("has a powersaving mode controlled by a tick box", function(){
@@ -27,4 +30,25 @@ describe("thermostat.html", function() {
     $("#power_saving_mode").click();
     expect(thermostat.powerSaving).toBe(false);
   });
+
+  it("it has a reset button which resets the thermostat to 20", function(){
+    $("#up").click();
+    $("#up").click();
+    $("#up").click();
+    expect("#temperature").toContainHtml("23");
+    expect("#temperature").toContainHtml(thermostat.temp);
+    $("#reset").click();
+    expect("#temperature").toContainHtml("20");
+    expect("#temperature").toContainHtml(thermostat.temp);
+  });
+
+  it("changes colour based on energy usage", function(){
+    $('#temperature').text(Math.floor(Math.random()*(18 - 10 + 1))+10);
+    expect($('#temperature').css("color")).toEqual("yellow");
+    $('#temperature').text(Math.floor(Math.random()*(25 - 18 + 1))+18);
+    expect('#temperature').toHaveCss({color: "green"});
+    $('#temperature').text(Math.floor(Math.random()*(35 - 25 + 1))+25);
+    expect('#temperature').toHaveCss({color: "red"});
+  });
+
 });
