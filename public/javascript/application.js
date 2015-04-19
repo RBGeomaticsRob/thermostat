f4a3fbd946722201caf532f0623e1853a8077058
RBGeomaticsRob <RBGeomaticsRob@users.noreply.github.com>
2015-04-19 01:35:56 +0100

    A working AJAX stub

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,69 +1,71 @@
 thermostat = new Thermostat();
 
+function loaddata(data) {
+$("#weatherapidata").text(data.main.temp);
+};
+
 $(document).ready(function(){
 
-        $('#London').click(function(){
-          var weatherAjax = $.ajax({
-            url: "http://api.openweathermap.org/data/2.5/weather",
-            data: {
-              q:"London",
-              units:"metric",
-              APPID:"d511992ca81fc06e08f2dfeff6e9bf93"
-            }
-            // success: function(data){
-            //   $('#weatherapidata').text(data.main.temp)
-            // }
-          });
-          weatherAjax.done(function(data){
-            $('#weatherapidata').text(data.main.temp)
-          });
-        });
+  $('#London').click(function(e){
+    e.preventDefault();
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather",
+      data: {
+        q:"London",
+        units:"metric",
+        APPID:"d511992ca81fc06e08f2dfeff6e9bf93"
+      },
+      success: function(data){
+        loaddata(data);
+      }
+    });
+  });
 
-        $('#temperature').text(thermostat.temp);
+  $('#temperature').text(thermostat.temp);
 
-        $('#up').click(function(){
-          thermostat.up();
-          changeTemp();
-        });
+  $('#up').click(function(){
+    thermostat.up();
+    changeTemp();
+  });
 
-        $('#down').click(function(){
-          thermostat.down();
-          changeTemp();
-        });
+  $('#down').click(function(){
+    thermostat.down();
+    changeTemp();
+  });
 
-        $('#power_saving_mode').change(function() {
-          if($('#power_saving_mode').is(":checked") && $('#temperature').text() > 23) {
-            thermostat.powerSavingOn();
-             changeTemp();
-            } else if ($('#power_saving_mode').is(":checked") && $('#temperature').text() < 24 ) {
-              thermostat.powerSavingOn();
-            } else {
-            thermostat.powerSavingOff();
-          };
-        });
+  $('#power_saving_mode').change(function() {
+    if($('#power_saving_mode').is(":checked") && $('#temperature').text() > 23) {
+      thermostat.powerSavingOn();
+       changeTemp();
+      } else if ($('#power_saving_mode').is(":checked") && $('#temperature').text() < 24 ) {
+        thermostat.powerSavingOn();
+      } else {
+      thermostat.powerSavingOff();
+    };
+  });
 
-        $('#reset').click(function(){
-          thermostat.reset();
-          changeTemp();
-        });
+  $('#reset').click(function(){
+    thermostat.reset();
+    changeTemp();
+  });
 
-        var changeColor = function() {
-          if ($('#temperature').text() < 18) {
-            $('#temperature').css('color', 'yellow');
-          } else if ($('#temperature').text() > 25 ) {
-            $('#temperature').css('color', 'red');
-          } else {
-            $('#temperature').css('color', 'green');
-          };
-        };
+  var changeColor = function() {
+    if ($('#temperature').text() < 18) {
+      $('#temperature').css('color', 'yellow');
+    } else if ($('#temperature').text() > 25 ) {
+      $('#temperature').css('color', 'red');
+    } else {
+      $('#temperature').css('color', 'green');
+    };
+  };
 
-        var changeTemp = function() {
-          $('#temperature').text(thermostat.temp);
-          changeColor();
-          $.ajax({url: "temperature_change", method: "POST", data: thermostat.temp
-          });
-        };
+  var changeTemp = function() {
+    $('#temperature').text(thermostat.temp);
+    changeColor();
+    $.ajax({url: "temperature_change", method: "POST", data: thermostat.temp
+    });
+  };
 
-        changeColor();
+  changeColor();
 
-      });
+});
